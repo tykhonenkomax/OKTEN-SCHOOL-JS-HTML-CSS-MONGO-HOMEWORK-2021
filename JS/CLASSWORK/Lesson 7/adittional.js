8a61f165b4f06b35ed316df8fe35da1320aae929
Max <95238323+tykhonenkomax@users.noreply.github.com>
2022-02-17 15:33:21 +0200

    на

--- JS/CLASSWORK/Lesson 7/adittional.js
+++ JS/CLASSWORK/Lesson 7/adittional.js
@@ -25,45 +25,89 @@
 // }
 // }
 
-function PersonData(id, phone, website, company) {
-    this.id = id;
-    this.phone = phone;
-    this.website = website;
-    this.company = company;
-};
+// function PersonData(id, phone, website, company) {
+//     this.id = id;
+//     this.phone = phone;
+//     this.website = website;
+//     this.company = company;
+// };
+//
+// function Id(name, username, email, address) {
+//     this.name = name;
+//     this.username = username;
+//     this.email = email;
+//     this.address = address;
+// };
+//
+// function Adress(street, suite, city, zipcode, geo) {
+//     this.street = street;
+//     this.suite = suite;
+//     this.zipcode = zipcode;
+//     this.geo = geo;
+// }
+//
+// function Geo(lat, lng) {
+//     this.lat = lat;
+//     this.lng = lng;
+// }
+//
+// function Company(name, catchPhrase, bs) {
+//     this.name = name;
+//     this.catchPhrase = catchPhrase;
+//     this.bs = bs;
+// }
+//
+//
+// let newPersonData = new PersonData(new Id('Leanne Graham', 'Sincere@april.biz', 'Sincere@april.biz',
+//         (new Adress('Kulas Light', 'Apt. 556', 'Gwenborough', '92998-3874', new Geo('-37.3159', '81.1496')))),
+//     '1-770-736-8031 x56442', 'hildegard.org', new Company('Romaguera-Crona', 'Multi-layered client-server neural-net',
+//         'harness real-time e-markets'));
+// console.log(newPersonData);
 
-function Id(name, username, email, address) {
-    this.name = name;
-    this.username = username;
-    this.email = email;
-    this.address = address;
-};
+//
+// class Task2 {
+//     constructor(nameTeg, descriptionTeg, atributsTeg) {
+//         this.nameTeg = nameTeg;
+//         this.descriptionTeg = descriptionTeg;
+//         this.atributsTeg = atributsTeg;
+//     }
+// }
+//
+// function Atribut(nameAtribut1, descriptionAtribut1, nameAtribut2, descriptionAtribut2, nameAtribut3, descriptionAtribut3) {
+//     this.nameAtribut1 = nameAtribut1;
+//     this.descriptionAtribut1 = descriptionAtribut1;
+//     this.nameAtribut2 = nameAtribut2;
+//     this.descriptionAtribut2 = descriptionAtribut2;
+//     this.nameAtribut3 = nameAtribut3;
+//     this.descriptionAtribut3 = descriptionAtribut3;
+// }
+//
+// let changeTask2 = new Task2('Тег <a>', 'Тег <a> является одним из важных элементов HTML и предназначен для создания ссылок.',
+//     new Atribut('accesskey', 'Активация ссылки с помощью комбинации клавиш.', 'coords', 'Устанавливает координаты активной области.',
+//         'download', 'Предлагает скачать указанный по ссылке файл.'));
+// console.log(changeTask2);
 
-function Adress(street, suite, city, zipcode, geo) {
-    this.street = street;
-    this.suite = suite;
-    this.zipcode = zipcode;
-    this.geo = geo;
+
+class Task3 {
+    constructor(nameTeg, descriptionTeg, atributsTeg) {
+        this.nameTeg = nameTeg;
+        this.descriptionTeg = descriptionTeg;
+        this.atributsTeg = atributsTeg;
+    }
 }
+    function Atribut3(name, description, name2, description2) {
+        this.name = name;
+        this.description = description;
+        this.name2 = name2;
+        this.description2 = description2;
 
-function Geo(lat, lng) {
-    this.lat = lat;
-    this.lng = lng;
-}
-
-function Company(name, catchPhrase, bs) {
-    this.name = name;
-    this.catchPhrase = catchPhrase;
-    this.bs = bs;
-}
+    };
 
 
-let newPersonData = new PersonData(new Id('Leanne Graham', 'Sincere@april.biz', 'Sincere@april.biz',
-        (new Adress('Kulas Light', 'Apt. 556', 'Gwenborough', '92998-3874', new Geo('-37.3159', '81.1496')))),
-    '1-770-736-8031 x56442', 'hildegard.org', new Company('Romaguera-Crona', 'Multi-layered client-server neural-net',
-        'harness real-time e-markets'));
-console.log(newPersonData);
-
+let resultTask3 = new Task3('Тег <div>', 'Элемент <div> является блочным элементом и предназначен для выделения ' +
+    'фрагмента документа с целью изменения вида содержимого.', new Atribut3('align', 'Задает выравнивание содержимого тега <div>.',
+    'title', 'Добавляет всплывающую подсказку к содержимому.'))
+console.log(resultTask3);
 
 
 // -  Створити функцію конструктор / клас  який описує об'єкт тегу
@@ -74,6 +118,8 @@ console.log(newPersonData);
 // Кожен атрибут описати як окремий який буде містити
 // -назву атрибуту
 // -опис дії атрибуту
+
+
 // інформацію брати з htmlbook.ru
 //
 // Таким чином описати теги
