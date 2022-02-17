e772f4b6460a08473bf12c334c4b9db338d65e2a
Max <95238323+tykhonenkomax@users.noreply.github.com>
2022-02-18 00:37:58 +0200

    ->

--- JS/CLASSWORK/Lesson 7/сlosures.js
+++ JS/CLASSWORK/Lesson 7/сlosures.js
@@ -1,3 +1,5 @@
+//---------------------------------Вариант №1---------------------------------------------------------------------------
+
 function UserCard(key) {
     this.key=key;
 }
@@ -17,23 +19,37 @@ console.log(card2);
 let card3 = new UserCard(3, new Key(100, 100, [], 3));
 console.log(card3);
 
+//---------------------------------Вариант №2---------------------------------------------------------------------------
 
-// let card1 = new UserCard(100, 100, [], 1);
-// console.log(card1)
-// let card2 = new UserCard(100, 100, [], 2);
-// let card3 = new UserCard(100, 100, [], 3);
-//
-// function getCardOptions(numberCard) {
-//
-//     if (numberCard === 1) {
-//         return card1;
-//     } else if (numberCard === 2) {
-//         return card2;
-//     } else {
-//         return card3;
-//     }
-// }
-// getCardOptions(1);
+
+function UserCard(balance,transactionLimit,historyLogs,key){
+    this.balance = balance;
+    this.transactionLimit = transactionLimit;
+    this.historyLogs = historyLogs;
+    this.key = key;
+}
+
+
+let card1 = new UserCard(100, 100, [], 1);
+console.log(card1)
+let card2 = new UserCard(100, 100, [], 2);
+console.log(card2)
+let card3 = new UserCard(100, 100, [], 3);
+console.log(card3)
+
+
+
+function getCardOptions(numberCard) {
+
+    if (numberCard === 1) {
+        return card1;
+    } else if (numberCard === 2) {
+        return card2;
+    } else {
+        return card3;
+    }
+}
+getCardOptions(1);
 
 
 
