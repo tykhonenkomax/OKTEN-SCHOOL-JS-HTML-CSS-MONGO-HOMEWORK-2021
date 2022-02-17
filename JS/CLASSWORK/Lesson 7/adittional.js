5b629edb08977ca45bc095b78c95bbf621ca0098
Max <95238323+tykhonenkomax@users.noreply.github.com>
2022-02-17 14:06:18 +0200

    на

---
+++ JS/CLASSWORK/Lesson 7/adittional.js
@@ -0,0 +1,99 @@
+// - Створити клас або функцію конструктор, за допомоги якої можна створювати об'єкти наступного вигляду.
+// Конструктор повинен приймати значення для кожної властивості, в т.ч і для властивостей внутрішніх об'єктів
+
+// {
+//     id: 1,
+//         name: 'Leanne Graham',
+//     username: 'Bret',
+//     email: 'Sincere@april.biz',
+//     address: {
+//     street: 'Kulas Light',
+//         suite: 'Apt. 556',
+//         city: 'Gwenborough',
+//         zipcode: '92998-3874',
+//         geo: {
+//         lat: '-37.3159',
+//             lng: '81.1496'
+//     }
+// },
+//     phone: '1-770-736-8031 x56442',
+//         website: 'hildegard.org',
+//     company: {
+//     name: 'Romaguera-Crona',
+//         catchPhrase: 'Multi-layered client-server neural-net',
+//         bs: 'harness real-time e-markets'
+// }
+// }
+
+function PersonData(id, phone, website, company) {
+    this.id = id;
+    this.phone = phone;
+    this.website = website;
+    this.company = company;
+};
+
+function Id(name, username, email, address) {
+    this.name = name;
+    this.username = username;
+    this.email = email;
+    this.address = address;
+};
+
+function Adress(street, suite, city, zipcode, geo) {
+    this.street = street;
+    this.suite = suite;
+    this.zipcode = zipcode;
+    this.geo = geo;
+}
+
+function Geo(lat, lng) {
+    this.lat = lat;
+    this.lng = lng;
+}
+
+function Company(name, catchPhrase, bs) {
+    this.name = name;
+    this.catchPhrase = catchPhrase;
+    this.bs = bs;
+}
+
+
+let newPersonData = new PersonData(new Id('Leanne Graham', 'Sincere@april.biz', 'Sincere@april.biz',
+        (new Adress('Kulas Light', 'Apt. 556', 'Gwenborough', '92998-3874', new Geo('-37.3159', '81.1496')))),
+    '1-770-736-8031 x56442', 'hildegard.org', new Company('Romaguera-Crona', 'Multi-layered client-server neural-net',
+        'harness real-time e-markets'));
+console.log(newPersonData);
+
+
+
+// -  Створити функцію конструктор / клас  який описує об'єкт тегу
+// Поля :
+//     -назва тегу ()
+// - опис його дій
+// - масив з атрибутами (2-3 атрибути максимум)
+// Кожен атрибут описати як окремий який буде містити
+// -назву атрибуту
+// -опис дії атрибуту
+// інформацію брати з htmlbook.ru
+//
+// Таким чином описати теги
+// -a
+// -div
+// -h1
+// -span
+// -input
+// -form
+// -option
+// -select
+// Приклад результуючого об'єкту
+// {
+//     titleOfTag: 'area',
+//         action: `Каждый элемент <area> определяет активные области изображения, которые являются ссылками...`,
+//     attrs: [
+//     {titleOfAttr: 'accesskey', actionOfAttr: 'Переход к области с помощью комбинации клавиш'},
+//     {/*some props and values*/},
+//     {/*...*/},
+//     {/*...*/},
+// ]
+//
+// }
