5a089c9316e5ce6900003cbce6da0a24bf0d3442
Max <95238323+tykhonenkomax@users.noreply.github.com>
2022-03-15 00:51:39 +0200

    ->>

---
+++ JS/CLASSWORK/Lesson 12 DOM/12.js
@@ -0,0 +1,46 @@
+//  // 1.
+// //  Отримати відповідь з цього ресурсу відповідь, та вивести в документ як в прикладі на занятті
+//  // https://jsonplaceholder.typicode.com/posts
+// //     зробити кнопку до кожного поста. при кліку на яку виводяться в окремий блок всі коментарі поточного поста
+
+fetch('https://jsonplaceholder.typicode.com/posts').then(responce => {
+    return responce.json();
+}).then(jsonResponce => {
+    let generalDiv = document.createElement('div');
+
+    for (const IterationJason of jsonResponce) {
+        let objecktDiv = document.createElement('div');
+        let btn1 = document.createElement('button');
+        objecktDiv.innerHTML = `
+        <h3>UserId: ${IterationJason.userId}</h3>
+        <h5>Id: ${IterationJason.id}<h5>
+        <p>TITLE: ${IterationJason.title}</p> <br>
+        <p>BODY: ${IterationJason.body}</p> <br>
+        `;
+
+
+        btn1.innerText = `Read The Comments Id: ${IterationJason.id}`;
+        btn1.onclick = () => {
+            fetch('https://jsonplaceholder.typicode.com/comments').then(responceComment => {
+                return responceComment.json();
+            }).then(jasonResponceComments => {
+                let commentDiv = document.createElement('div');
+                for (const posts of jasonResponceComments) {
+                    commentDiv.innerHTML = `
+                    email: ${posts.email}<br>
+                    name: ${posts.name}<br>
+                    `;
+
+                    let commentsResult = document.createElement('div')
+                    commentsResult.innerHTML=commentDiv;
+                    objecktDiv.append(commentsResult)
+
+                }
+            })
+        }
+        generalDiv.append(btn1)
+        generalDiv.append(objecktDiv);
+        document.body.append(generalDiv);
+
+    }
+});
