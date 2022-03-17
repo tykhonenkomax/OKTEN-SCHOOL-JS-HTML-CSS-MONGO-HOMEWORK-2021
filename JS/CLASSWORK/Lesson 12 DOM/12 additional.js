1367aad6fa469ec732f56615f34bd271473b26c5
Max <95238323+tykhonenkomax@users.noreply.github.com>
2022-03-18 00:00:50 +0200

    ->>

--- JS/CLASSWORK/Lesson 12 DOM/12 additional.js
+++ JS/CLASSWORK/Lesson 12 DOM/12 additional.js
@@ -4,38 +4,38 @@
 //     кожному елементу юзера створити кнопку, при клику на яку в окремий блок виводяться всі пости поточного юзера.
 //     Кожному елементу post створити кнопку, при клику на яку в окремий блок виводяться всі коментарі поточного поста
 
-fetch('https://jsonplaceholder.typicode.com/users').then(responce => {
-    return responce.json()
-}).then(responceJson => {
-    for (let objectIter of responceJson) {
-        for (let objectIter2 in objectIter) {
-            // console.log(objectIter)
-            let divGeneral = document.createElement('div');
-            if(objectIter2 != 'address' && objectIter2 != 'company') {
-               // console.log(objectIter.address.geo)
-                let div1 = document.createElement('div')
-                div1.innerHTML = `
-                ${objectIter2}: ${objectIter[objectIter2]}`
+fetch('https://jsonplaceholder.typicode.com/users').then((response)=>{
+    return response.json();
+}).then((data)=>{
+let divGeneral = document.createElement('div')
+    for (const object1 of data) {
+
+        for (const Key1 in object1) {
+
+            if (typeof object1[Key1] !== 'object') {
+
+                let div1 = document.createElement('div');
+                div1.innerHTML=`
+                ${Key1}: ${object1[Key1]}
+                `
                 divGeneral.append(div1)
-                }else if (objectIter2 === 'address' && objectIter2 === 'company'){
-                for (const divGeneralElement in objectIter.address.geo) {
-                    console.log(divGeneralElement)
-                    let div2 = document.createElement('div')
+             }else{
+                for (const Key2 in object1[Key1]) {
+                    let div2 = document.createElement('div');
+
                     div2.innerHTML=`
-                    ${divGeneralElement}:${objectIter.address.geo[divGeneralElement]}
-                    `
-                    div1.append(div2);
+                ${Key2}: ${object1[Key1][Key2]}
+                `
+                    divGeneral.append(div2)
 
                 }
+
             }
 
-
-
-
-
-
-
-                document.body.append(divGeneral)
     }
 
-}})
+}
+    document.body.append(divGeneral);
+})
+
+
