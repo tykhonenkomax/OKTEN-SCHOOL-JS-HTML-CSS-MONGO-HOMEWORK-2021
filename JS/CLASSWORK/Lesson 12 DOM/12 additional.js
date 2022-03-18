3e04b5e6a686db13bd6cbac45370757b1d4569e5
Max <95238323+tykhonenkomax@users.noreply.github.com>
2022-03-18 23:11:21 +0200

    ->>

--- JS/CLASSWORK/Lesson 12 DOM/12 additional.js
+++ JS/CLASSWORK/Lesson 12 DOM/12 additional.js
@@ -11,32 +11,44 @@ fetch('https://jsonplaceholder.typicode.com/users').then((response) => {
     for (const object1 of data) {
 
         for (const Key1 in object1) {
+            console.log(Key1)
 
             if (typeof object1[Key1] !== 'object') {
-
                 let div1 = document.createElement('div');
                 div1.innerHTML = `
-                ${Key1}: ${object1[Key1]}
-                `
+                ${Key1}: ${object1[Key1]}`
                 divGeneral.append(div1)
             } else {
+                let div3 = document.createElement('div');
+                div3.innerHTML = `${Key1}:`
+                divGeneral.append(div3)
                 for (const Key2 in object1[Key1]) {
 
+
+                    if (typeof object1[Key1][Key2] !=='object') {
+
+                        let div5 = document.createElement('div');
+                        div5.innerHTML = `${Key2}:${object1[Key1][Key2]}`
+                        divGeneral.append(div5)
+                    }
+
                     let div2 = document.createElement('div');
-                    div2.innerHTML = `
-                ${Key2}: ${object1[Key1][Key2]}
-                `
+                    div2.innerHTML = `${Key2}:`
+
                     if (typeof object1[Key1][Key2] === 'object') {
 
                         for (const key2Key in object1[Key1][Key2]) {
-                            div2.innerHTML = `
+
+                            let div4 = document.createElement('div')
+                            div4.innerHTML = `
                             ${key2Key}: ${object1[Key1][Key2][key2Key]}
                             `
-
+                            div2.append(div4)
                         }
                         divGeneral.append(div2)
                     }
                 }
+
             }
 
         }
